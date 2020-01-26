68351859ad2d578eb401ccb36e657947f4fba22d
vinit <vinit@observe.ai>
2020-01-26 16:33:15 +0530

    Scuba naut inialt commit

---
+++ src/constants/data.js
@@ -0,0 +1,5 @@
+export default {
+  WHY_SCUBA: "WHY SCUBA DIVE?",
+  SCUBA_ANS:
+    "DId you know that 70% of the planet is covered with water and over 21,000 species of ﬁsh live in it? Scuba Diving gives you that freedom to explore those unexplored underwater territories and the best part is you don't have to physically exert yourself in the process. A lot of Scuba Divers agree with us that the very act of staying under the water tends to be meditative and relieves stress from our day to day life. Do sign up for our Open Water Diver program to know how this is going to change your life."
+};
--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,5 +1,9 @@
 import './home.scss';
 import Rotator from '../../js/Rotator';
+import Data from '../../constants/data';
+import source from './home.handlebars';
 
 const rotator = new Rotator();
 rotator.rotate(document.getElementsByClassName('logo')[0], 500);
+const template = Handlebars.compile(source);
+template(Data);
